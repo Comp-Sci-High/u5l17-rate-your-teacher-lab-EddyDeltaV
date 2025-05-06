8aed74ee70539e9dfe74e4a0e57ed9842f23481f
EddyDeltaV <Eduardo.santos25@compscihigh.org>
2025-05-06 13:16:25 +0000

    I have finished please regrade

--- public/ratingScript.js
+++ public/ratingScript.js
@@ -2,18 +2,18 @@ const { object } = require("webidl-conversions")
 
 // Add an event listener to the form that upon submits creates a new rating
 const form = document.querySelector("form")
-form.addEventListener("submit", async(e)=>{
-    e.preventDefault()
-    const teacherData = new FormData(form)
-    const reqBody = object.fromEntries(teacherData)
-    const response = await fetch("/add/rating",{
-       method:"POST",
-       headers:{
-        "content-Type":"application/Json"
-       },
-       body:JSON.stringify(reqBody)
-    })
-    const data = await response.json()
-    console.log(data)
+form.addEventListener("submit", async (e)=>{
+e.preventDefault()
+const formData = new FormData(form)
+const reqBody = Object.fromEntries(formData)
+const response = await fetch("/add/rating", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(reqBody),
+ 
+  });
+const data = await response.json()
+console.log(data)
+window.location.href = "/"
 })
 // When the rating is created redirect to the ratings page
--- public/teacherScript.js
+++ public/teacherScript.js
@@ -1,20 +1,20 @@
 // Add an event listener to the form that upon submits creates a new teacher
-const { object } = require("webidl-conversions")
-
-
 const form = document.querySelector("form")
-form.addEventListener("submit", async(e)=>{
-    e.preventDefault()
-    const teacherData = new FormData(form)
-    const reqBody = object.fromEntries(teacherData)
-    const response = await fetch("/add/teacher",{
-       method:"POST",
-       headers:{
-        "content-Type":"application/Json"
-       },
-       body:JSON.stringify(reqBody)
-    })
-    const data = await response.json()
-    console.log(data)
+
+form.addEventListener("submit", async (e)=>{
+e.preventDefault()
+const formData = new FormData(form)
+const reqBody = Object.fromEntries(formData)
+const response = await fetch("/add/teacher", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(reqBody),
+ 
+  });
+const data = await response.json()
+console.log(data)
+window.location.href = "/"
 })
+
+
 // When the teacher is created redirect to the teachers page
